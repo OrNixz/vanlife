298dea6f8834e6921adaa39560704856150f4a67
OrNixz <ramadhanafif172@gmail.com>
2024-06-01 14:55:18 +0700

    feat: Implement HostVans page with data fetching and rendering

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function HostVans() {
-    return <h1>Host Vans goes here</h1>;
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const hostVansElements = vans.map((van) => (
+    <Link
+      to={`/host/vans/${van.id}`}
+      key={van.id}
+      className="host-van-link-wrapper"
+    >
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+      </div>
+    </Link>
+  ));
+
+  return (
+    <section>
+      <h1 className="host-vans-title">Your listed vans</h1>
+      <div className="host-vans-list">
+        {vans.length > 0 ? (
+          <section>{hostVansElements}</section>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
+    </section>
+  );
 }
