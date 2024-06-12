ef3a3e3dc05dcc45add5f7144484ee38263ff7e8
OrNixz <ramadhanafif172@gmail.com>
2024-06-12 16:36:00 +0700

    refactor: Enhance Dashboard page with van listing and state handling

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,10 +1,79 @@
-import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs";
+import { getHostVans } from "../../../api";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  const [vans, setVans] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getHostVans()
+      .then((data) => setVans(data))
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  function renderVanElements(vans) {
+    const hostVansElements = vans.map((van) => (
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+        <Link to={`vans/${van.id}`}>View</Link>
+      </div>
+    ));
+
+    return (
+      <div className="host-vans-list">
+        <section>{hostVansElements}</section>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error: {error.message}</h1>;
+  }
+
   return (
     <>
-      <h1>Dashboard goes here</h1>
-      <Outlet />
+      <section className="host-dashboard-earnings">
+        <div className="info">
+          <h1>Welcome!</h1>
+          <p>
+            Income last <span>30 days</span>
+          </p>
+          <h2>$2,260</h2>
+        </div>
+        <Link to="income">Details</Link>
+      </section>
+      <section className="host-dashboard-reviews">
+        <h2>Review score</h2>
+        <BsStarFill className="star" />
+        <p>
+          <span>5.0</span>/5
+        </p>
+        <Link to="reviews">Details</Link>
+      </section>
+      <section className="host-dashboard-vans">
+        <div className="top">
+          <h2>Your listed vans</h2>
+          <Link to="vans">View All</Link>
+        </div>
+        {loading && !vans ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>{renderVanElements(vans)}</>
+        )}
+      </section>
     </>
   );
 }
