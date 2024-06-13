f23c2feb12294b2692b590faa2f837475a446c1b
OrNixz <ramadhanafif172@gmail.com>
2024-06-13 17:16:12 +0700

    refactor: Migrate van data fetching to use Firebase Firestore

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAr8k2I6zKAQSuACQpyeINbQmVkqkZB5R0",
@@ -7,29 +7,37 @@ const firebaseConfig = {
   projectId: "vanlife-ornixz",
   storageBucket: "vanlife-ornixz.appspot.com",
   messagingSenderId: "200727460596",
-  appId: "1:200727460596:web:b8dffa13727d22d5d5fca1"
+  appId: "1:200727460596:web:b8dffa13727d22d5d5fca1",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve(), ms));
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const snapshot = await getDocs(vansCollectionRef);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
 }
 
-export async function getVans(id) {
-  const url = id ? `/api/vans/${id}` : "/api/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
-}
+// export async function getVans(id) {
+//   const url = id ? `/api/vans/${id}` : "/api/vans";
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw {
+//       message: "Failed to fetch vans",
+//       statusText: res.statusText,
+//       status: res.status,
+//     };
+//   }
+//   const data = await res.json();
+//   return data.vans;
+// }
 
 export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
